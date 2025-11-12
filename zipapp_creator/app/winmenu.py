5f2a2c0fdcb149a6884f78516650c74bb7320b72
zimolab <zimolab@aliyun.com>
2025-11-12 23:46:20 +0800

    move translatable strings to messages.py

--- zipapp_creator/app/winmenu.py
+++ zipapp_creator/app/winmenu.py
@@ -7,7 +7,8 @@ from pyguiadapterlite.components.textview import SimpleTextViewer
 
 from .aboutdlg import AboutDialog
 from .. import assets
-from ..common import trfunc, get_appconfig
+from ..common import get_appconfig
+from ..messages import messages
 from ..utils import move_to_center_of
 
 LICENSE_FILE = "LICENSE"
@@ -18,10 +19,13 @@ _menus = None
 
 # noinspection PyUnusedLocal
 def _on_action_save_params(window: FnExecuteWindow, action: Action):
-    tr = trfunc()
+    msgs = messages()
     save_filepath = window.select_save_file(
-        title=tr("Save Parameters"),
-        filetypes=[(tr("JSON Files"), "*.json"), (tr("All Files"), "*")],
+        title=msgs.MSG_SAVE_PARAMS_TITLE,
+        filetypes=[
+            (msgs.MSG_JSON_FILE_FILTER, "*.json"),
+            (msgs.MSG_ALL_FILES_FILTER, "*"),
+        ],
     )
     if not save_filepath:
         return
@@ -35,21 +39,21 @@ def _on_action_save_params(window: FnExecuteWindow, action: Action):
             json.dump(param_values, f, indent=2, ensure_ascii=False)
     except BaseException as e:
         window.show_error(
-            tr("Failed to save parameters to file: {}").format(save_filepath),
-            detail=str(e),
+            msgs.MSG_SAVE_PARAMS_ERROR.format(save_filepath), detail=str(e)
         )
     else:
-        window.show_information(
-            tr("Parameters saved to file: {}").format(save_filepath)
-        )
+        window.show_information(msgs.MSG_SAVE_PARAMS_SUCCESS.format(save_filepath))
 
 
 # noinspection PyUnusedLocal
 def _on_action_load_params(window: FnExecuteWindow, action: Action):
-    tr = trfunc()
+    msgs = messages()
     load_filepath = window.select_open_file(
-        title=tr("Load Parameters"),
-        filetypes=[(tr("JSON Files"), "*.json"), (tr("All Files"), "*")],
+        title=msgs.MSG_LOAD_PARAMS_TITLE,
+        filetypes=[
+            (msgs.MSG_JSON_FILE_FILTER, "*.json"),
+            (msgs.MSG_ALL_FILES_FILTER, "*"),
+        ],
     )
     if not load_filepath:
         return
@@ -59,21 +63,18 @@ def _on_action_load_params(window: FnExecuteWindow, action: Action):
             param_values = json.load(f)
     except BaseException as e:
         window.show_error(
-            tr("Failed to load parameters from file: {}").format(load_filepath),
+            msgs.MSG_LOAD_PARAMS_ERROR.format(load_filepath),
             detail=str(e),
         )
     else:
         ret = window.set_parameter_values(param_values)
         if not ret:
-            window.show_error(
-                tr("Invalid parameters in file: {}").format(load_filepath)
-            )
+            window.show_error(msgs.MSG_INVALID_PARAMS_IN_FILE.format(load_filepath))
         else:
-            window.show_information(
-                tr("Parameters loaded from file: {}").format(load_filepath)
-            )
+            window.show_information(msgs.MSG_LOAD_PARAMS_SUCCESS.format(load_filepath))
 
 
+# noinspection PyUnusedLocal
 def _on_action_exit(window: FnExecuteWindow, action: Action):
     window.close()
 
@@ -87,24 +88,22 @@ def _on_action_always_on_top(window: FnExecuteWindow, action: Action):
 
 # noinspection PyUnusedLocal
 def _on_action_about(window: FnExecuteWindow, action: Action):
-    tr = trfunc()
-    window.show_custom_dialog(AboutDialog, title=tr("About"))
+    msgs = messages()
+    window.show_custom_dialog(AboutDialog, title=msgs.MSG_ABOUT_TITLE)
 
 
 # noinspection PyUnusedLocal
 def _on_action_license(window: FnExecuteWindow, action: Action):
-    tr = trfunc()
+    msgs = messages()
     try:
         text = assets.read_asset_text(LICENSE_FILE)
     except Exception as e:
         print(e, file=sys.stderr)
-        window.show_error(
-            message="This project is under the MIT license.", title=tr("Error")
-        )
+        window.show_error(message=msgs.MSG_LICENSE_HINT)
         return
 
     viewer = SimpleTextViewer(
-        title=tr("License"),
+        title=msgs.MSG_LICENSE_TITLE,
         width=800,
         height=600,
     )
@@ -116,12 +115,7 @@ def _on_action_license(window: FnExecuteWindow, action: Action):
 # noinspection PyUnusedLocal
 def _on_action_edit_config(window: FnExecuteWindow, action: Action):
     app_config = get_appconfig()
-    tr = trfunc()
-    window.show_information(
-        tr(
-            "Changes to the configuration file will be take effect after restarting the program!"
-        )
-    )
+    window.show_information(message=messages().MSG_EDIT_APP_CONFIG_HINT)
     try:
         app_config.open_in_editor()
     except Exception as e:
@@ -129,45 +123,47 @@ def _on_action_edit_config(window: FnExecuteWindow, action: Action):
 
 
 def _create_file_actions() -> list[Action]:
-    tr = trfunc()
+    msgs = messages()
+
     return [
-        Action(tr("Save Parameters"), _on_action_save_params),
-        Action(tr("Load Parameters"), _on_action_load_params),
+        Action(msgs.MSG_ACTION_SAVE_PARAMS, _on_action_save_params),
+        Action(msgs.MSG_ACTION_LOAD_PARAMS, _on_action_load_params),
         Separator(),
-        Action(tr("Exit"), _on_action_exit),
+        Action(msgs.MSG_ACTION_EXIT, _on_action_exit),
     ]
 
 
 def _create_view_actions() -> list[Action]:
-    tr = trfunc()
+    msgs = messages()
+
     app_config = get_appconfig()
     return [
         Action(
-            tr("Always on Top"),
+            msgs.MSG_ACTION_ALWAYS_ON_TOP,
             on_triggered=_on_action_always_on_top,
             checkable=True,
             initial_checked=app_config.always_on_top,
         ),
         Separator(),
-        Action(tr("Edit Config"), on_triggered=_on_action_edit_config),
+        Action(msgs.MSG_ACTION_EDIT_CONFIG, on_triggered=_on_action_edit_config),
     ]
 
 
 def _create_help_actions() -> list[Action]:
-    tr = trfunc()
+    msgs = messages()
     return [
-        Action(tr("About"), on_triggered=_on_action_about),
-        Action(tr("License"), on_triggered=_on_action_license),
+        Action(msgs.MSG_ACTION_ABOUT, on_triggered=_on_action_about),
+        Action(msgs.MSG_ACTION_LICENSE, on_triggered=_on_action_license),
     ]
 
 
 def create_menus() -> list[Menu]:
     global _menus
     if not _menus:
-        tr = trfunc()
+        msgs = messages()
         _menus = [
-            Menu(tr("File"), actions=_create_file_actions()),
-            Menu(tr("View"), actions=_create_view_actions()),
-            Menu(tr("Help"), actions=_create_help_actions()),
+            Menu(msgs.MSG_MENU_FILE, actions=_create_file_actions()),
+            Menu(msgs.MSG_MENU_VIEW, actions=_create_view_actions()),
+            Menu(msgs.MSG_MENU_HELP, actions=_create_help_actions()),
         ]
     return _menus
--- zipapp_creator/messages.py
+++ zipapp_creator/messages.py
@@ -4,6 +4,32 @@ class _Messages(object):
 
         tr = trfunc()
 
+        self.MSG_SAVE_PARAMS_TITLE = tr("Save Parameters")
+        self.MSG_LOAD_PARAMS_TITLE = tr("Load Parameters")
+        self.MSG_JSON_FILE_FILTER = tr("JSON Files")
+        self.MSG_ALL_FILES_FILTER = tr("All Files")
+        self.MSG_SAVE_PARAMS_SUCCESS = tr("Parameters saved to file: {}")
+        self.MSG_LOAD_PARAMS_SUCCESS = tr("Parameters loaded from file: {}")
+        self.MSG_SAVE_PARAMS_ERROR = tr("Failed to save parameters to file: {}")
+        self.MSG_LOAD_PARAMS_ERROR = tr("Failed to load parameters from file: {}")
+        self.MSG_INVALID_PARAMS_IN_FILE = tr("Invalid parameters in file: {}")
+        self.MSG_ABOUT_TITLE = tr("About")
+        self.MSG_LICENSE_TITLE = tr("License")
+        self.MSG_LICENSE_HINT = tr("This project is under the MIT license.")
+        self.MSG_EDIT_APP_CONFIG_HINT = tr(
+            "Changes to the configuration file will be take effect after restarting the program!"
+        )
+        self.MSG_ACTION_SAVE_PARAMS = tr("Save Parameters")
+        self.MSG_ACTION_LOAD_PARAMS = tr("Load Parameters")
+        self.MSG_ACTION_EXIT = tr("Exit")
+        self.MSG_ACTION_ALWAYS_ON_TOP = tr("Always on Top")
+        self.MSG_ACTION_ABOUT = tr("About")
+        self.MSG_ACTION_LICENSE = tr("License")
+        self.MSG_ACTION_EDIT_CONFIG = tr("Edit Config")
+        self.MSG_MENU_FILE = tr("File")
+        self.MSG_MENU_VIEW = tr("View")
+        self.MSG_MENU_HELP = tr("Help")
+
 
 _messages = None
 
