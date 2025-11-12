87544557290628b808466251f1c7f52f20944628
zimolab <zimolab@aliyun.com>
2025-11-12 23:24:01 +0800

    impl before_window_close callback

--- zipapp_creator/app/winevents.py
+++ zipapp_creator/app/winevents.py
@@ -8,6 +8,8 @@ def after_window_create(window: FnExecuteWindow):
     window.set_always_on_top(app_config.always_on_top)
 
 
+# noinspection PyUnusedLocal
 def before_window_close(window: FnExecuteWindow):
-    print("Window closing")
+    app_config = get_appconfig()
+    app_config.save(encoding="utf-8", ensure_ascii=False, indent=2)
     return True
